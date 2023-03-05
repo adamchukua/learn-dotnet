6208fb569603e25756a6e31c766f02ad70b8a4bf
thegradle <adamchuk.v.1@gmail.com>
2023-03-05 17:29:34 +0200

    Add lab4

---
+++ Lab4/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        private int Age { get; set; }
+        protected double Height { get; set; }
+        internal bool IsMarried { get; set; }
+        public DateTime Created { get; set; }
+
+        public Person(string name, int age, double height, bool isMarried, DateTime created)
+        {
+            Name = name;
+            Age = age;
+            Height = height;
+            IsMarried = isMarried;
+            Created = created;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello, my name is {Name}.");
+        }
+
+        private void HaveBirthday()
+        {
+            Age++;
+            Console.WriteLine($"Happy Birthday! I am now {Age} years old.");
+        }
+
+        protected bool IsTall()
+        {
+            return Height >= 6.0;
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Stepan", 18, 187.5, false, new DateTime(2023, 3, 5));
+
+            Type personType = person.GetType();
+            TypeInfo typeInfo = personType.GetTypeInfo();
+
+            Console.WriteLine("********MemberInfo********");
+
+            MemberInfo[] memberInfoList = personType.GetMembers();
+            foreach (MemberInfo memberInfo in memberInfoList) { Console.WriteLine(memberInfo); }
+
+            Console.WriteLine("********FieldInfo********");
+
+            FieldInfo[] fieldInfoList = personType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance
+                | BindingFlags.Public);
+            foreach (FieldInfo fieldInfo in fieldInfoList) { Console.WriteLine(fieldInfo); }
+
+            Console.WriteLine("********MethodInfo********");
+
+            IEnumerable<MethodInfo> methods = typeInfo.DeclaredMethods;
+            foreach (MethodInfo methodInfo in methods)
+            {
+                Console.WriteLine(methodInfo.DeclaringType.Name + ": " + methodInfo.Name);
+            }
+
+            Console.WriteLine("********MethodInfo GetMethod********");
+
+            MethodInfo sayHelloMethod = personType.GetMethod("SayHello");
+            string sayHelloMethodResult = (string)sayHelloMethod.Invoke(person, null);
+            Console.WriteLine(sayHelloMethodResult);
+
+            Console.ReadLine();
+        }
+    }
+}
